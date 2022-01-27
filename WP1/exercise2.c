8500e2f79c725d1e403da4c1254636e37830f10f
Karl Stahre <60395651+karlstahre@users.noreply.github.com>
2022-01-27 08:04:12 +0100

    Updated solution

--- WP1/exercise2.c
+++ WP1/exercise2.c
@@ -1,58 +1,26 @@
-/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
-	Run program with encryption key argument
-	User enters a text
-	Program read text input
-	Encrypt text using the encryption parameter
-	Loop over letters in text, if alphabet character, then ROT13
-	Return encrypted text to user
-	Repeat until EOF
-
-*/
-#include<stdio.h>
-#include<stdlib.h>
-#include<ctype.h>
-
-void rotate(char letter, int encryptionKey, char outputChar)
+int main(int argc, char* argv[])
 {
-	int number = (int)letter;
+    int encryptionKey = atoi(argv[1]);
 
-	if (isupper(number))
-	{
-		outputChar = (char)((number - 65 + encryptionKey) % 26) + 65;
-	}
-	else if (islower(number))
-	{
-		outputChar = (char)((number - 97 + encryptionKey) % 26) + 97;
-	}
-}
+    char input[1000];
 
-//return type here???
-void encrypt(char* input, int encryptionKey, char* output)
-{
-	for (int i = 0; i < sizeof input; i++)
-	{
-		//printf("%d\n", sizeof input);
-		rotate(input[i], encryptionKey, output[i]);
-	}
-}
+    fgets(input, sizeof(input), stdin);
 
-void main(int argc, char* argv[])
-{
-	int boolean = 1;
-	printf("%s %s", "program started", argv[1]);
-
-//while(boolean){
-
-	char input[10];
-	char output[1000];
-	scanf_s("%s\n", input);
-	int encryptionKey = atoi(argv[1]);
-	encrypt(input, encryptionKey, output);
-	printf("%s\n", output);
-	//}
-	/*for (int i = 0; i <10; i++)
-		printf("%s\n", output[i]);
-	*/
-	
+    for (int i = 0; i < strlen(input); i++)
+    {
+        if (isupper(input[i]))
+        {
+            input[i] = ((input[i] - 65 + encryptionKey) % 26) + 65;
+        }
+        else if (islower(input[i]))
+        {
+            input[i] = ((input[i] - 97 + encryptionKey) % 26) + 97;
+        }
+    }
+    printf("%s", input);
 }
