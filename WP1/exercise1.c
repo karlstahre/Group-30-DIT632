545089fcd39fe3b34f5f77b44ddd54d2eb3eec9c
Karl Stahre <60395651+karlstahre@users.noreply.github.com>
2022-01-27 09:23:23 +0100

    Adds comments to code in exercise 1

--- WP1/exercise1.c
+++ WP1/exercise1.c
@@ -1,41 +1,59 @@
 #include <stdio.h>
 
-//defines macros
+/* These lines are existing sentences that will be printed by the program once the user
+	enters a number between 1 and 5. Define are defined as macros on top of the rest of
+	the code to be referenced further down. */
 #define SENTENCE1 "Hejsan morsan\n\n"
 #define SENTENCE2 "Hejsan stabben\n\n"
 #define SENTENCE3 "Här är brev från älsklingsgrabben\n\n"
 #define SENTENCE4 "Det är bra här i kolonien\n\n"
 #define SENTENCE5 "Vi är 28 gangstergrabbar i en stor barrack\n\n"
 
+/* This program urges the user to enter a number between 1 and 5, which it stores in an
+int variable and then prints any of the five sentences defined as macros above, dependent
+on which number the user enters. Entered in order the program prints the first verse of a
+famous Swedish folk song. */
 void main(void) {
 
+	/* This is the variable to store the user input */
 	int input;
 
-	//loops over the following print statement as long as user input is a number between 1 and 5
+	/* The do-while loop executes the code inside its scope once and then over and over
+	until the user provides an input that does not fulfil the condition of being an integer
+	between 1 and 5 */
 	do {
 
+		/* This print method urges the user to enter a number between 1 and 5 */
 		printf("Please enter a number between 1 - 5\n\n");
 
-		//handles user input and stores it in global variable "input"
-		scanf("%d", &input);
+		/* This method reads the user input and stores it in the input variable defined
+		above */
+		scanf_s("%d", &input);
 
-		//if statement to return different sentences based on different user input
+		/* This if statement checks if the input variable satisfies any of the conditions,
+		i.e. if the user entered any of the numbers between 1 and 5 allowed */
 		if (input == 1) {
+			/* If the input is 1 the program prints the first sentence to the user */
 			printf(SENTENCE1);
 		}
 		else if (input == 2) {
+			/* If the input is 2 the program prints the second sentence to the user */
 			printf(SENTENCE2);
 		}
 		else if (input == 3) {
+			/* If the input is 3 the program prints the third sentence to the user */
 			printf(SENTENCE3);
 		}
 		else if (input == 4) {
+			/* If the input is 4 the program prints the fourth sentence to the user */
 			printf(SENTENCE4);
 		}
 		else if (input == 5) {
+			/* If the input is 5 the program prints the fifth sentence to the user */
 			printf(SENTENCE5);
 		}
 
-		//exits program if user input does not satisfy condition
+	/* If the condition that the input should be a number between 1 and 5 is not satisfied,
+	then the loop is exited and the program terminates */
 	} while (input > 0 && input < 6);
 }
