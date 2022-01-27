210cc1f5492fe41217c8d54ddfde8d476818aa09
Gianmarco Iachella <81154210+iachella@users.noreply.github.com>
2022-01-27 16:14:52 +0100

    Update exercise3.c with todos solved
    todos solved after Krasen's feedback

--- WP1/exercise3.c
+++ WP1/exercise3.c
@@ -6,11 +6,14 @@
 #include<time.h>    // used for srand()
 
 // define macros
-#define MAX_NUMBER 100 // Highest value that the user can guess and maximum times the user can guess
+#define MAX_NUMBER 100 // Highest value that the user can guess
+#define MIN_NUMBER 1   // Lowest value that the user can guess
+#define MAX_TRIES 5    // Maximum number of guesses before game ends
 #define TOO_LOW "Your guess is too low \n"
 #define TOO_HIGH "Your guess is too high \n"
 #define PLAY_AGAIN "Do you want to play again? Press 1 for yes \n"
 #define GUESS "Guess a number from 1-100 \n"
+#define ERROR "Please provide a number from 1-100 \n"
 
 // ------ Function declarations ----------
 
@@ -27,11 +30,11 @@ int main(void)
 	srand(time(0));
 
 	int randomNumber = rand() % MAX_NUMBER + 1; // rand() generates a pseudo-random number, remainder of 100 + 1 to get a number between 1 and 100
-	int input;			 // User input from scanf
+	int input = 0;			 // User input from scanf
 	int nrOfGuesses = 0; // How many times the user has guessed, default 0
 	int play = 1;		 // As long as play is 1 the game will play, default 1
 
-	// Method call
+	// Function call
 	playGame(randomNumber, input, nrOfGuesses, play);
 }
 
@@ -43,19 +46,22 @@ void playGame(int randomNumber, int input, int nrOfGuesses, int play)
 		// Display a sentence asking the user for a number between 1-100
 		printf("%s", GUESS);
 		// Scan the user input
-		scanf("%d", &input);
-
-		nrOfGuesses++; 	// Number of guesses increases by 1 by each guess
+		scanf_s("%d", &input);
 
 		// Handles case when user input is a correct guess
-		if (input == randomNumber)
+		if (input > MAX_NUMBER || input < MIN_NUMBER)
 		{
+			printf("%s", ERROR);
+		}
+		else if (input == randomNumber)
+		{
+			nrOfGuesses++; 	// Number of guesses increases by 1 by each guess
 			// Display a message when the user wins and display how many times they guessed in that game
 			printf("%s %d %s", "You have guessed", nrOfGuesses, "times and your guess is correct \n");
 			// Display a question if the user wants to play again
 			printf(PLAY_AGAIN);
 			// Scan the user input
-			scanf("%d", &input);
+			scanf_s("%d", &input);
 
 			// Handles case when the user wants to play again
 			if (input == 1)
@@ -80,17 +86,18 @@ void playGame(int randomNumber, int input, int nrOfGuesses, int play)
 		{
 			// Display message that the guess is too low
 			printf(TOO_LOW);
+			nrOfGuesses++; 	// Number of guesses increases by 1 by each guess
 		}
 		// Handles case when user input guess is higher than correct answer
 		else if (input > randomNumber)
 		{
 			// Display message that the guess is too high
 			printf(TOO_HIGH);
+			nrOfGuesses++; 	// Number of guesses increases by 1 by each guess
 		}
 
 		// While will happen when:
 		// 1. It's the first round or the user wants to play again after one round
 		// 2. Max number of guesses is smaller as max number
-		// 3. User input is between 1 to 100
-	} while (play == 1 && MAX_NUMBER > nrOfGuesses && input >= 0 && input <= 100);
+	} while (play == 1 && nrOfGuesses < MAX_TRIES);
 }
