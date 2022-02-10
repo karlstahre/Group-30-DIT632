8fefe9be5ac78b97071dfdcd9b6fab1ee8dd6abd
Karl Stahre <60395651+karlstahre@users.noreply.github.com>
2022-02-10 12:15:28 +0100

    Adds comments

--- WP3/exercise2_part1.c
+++ WP3/exercise2_part1.c
@@ -1,77 +1,70 @@
-//TODO: add comments and header with our names
+// (C) Maja Linder, Karl Stahre, Gianmarco Iachella group: 30 (2022)
+// Work package 3
+// Exercise 2 part 1
+// Submission code:
+
 /* constants */
-const int TEMP_SENSOR = 0; 	// Analog pin for temperature sensor
-const int LIGHT_SENSOR = 1;	// Analog pin for light sensor
-const int RED_LED = 13;		// Digital pin for red led
+const int TEMP_SENSOR = 0; 		// Analog pin for temperature sensor
+const int LIGHT_SENSOR = 1;		// Analog pin for light sensor
+const int RED_LED = 13;			// Digital pin for red led
 const int GREEN_LED = 12;		// Digital pin for green led
 const int BLUE_LED = 11;		// Digital pin for blue led
-const int OFFSET = 50;
-const int ERROR = 8000;
-const int ALL_LOW = B00000000;
-
-// offset used for volt to celsius conversion
-const double K = 7.6923;	// Constant used for light to percentage conversion
+const int OFFSET = 50;			// Used for conversion from volt to Celsius
+const int ERROR = 8000;			// Used for error handling
+const int ALL_LOW = B00000000;		// Used to reset all pins to LOW
+const double K = 7.6923;		// Used for light to percentage conversion
 
 // variable numbers
-double currentTemp = 0;	// Initialize temperature value
-double currentLight = 0;	// Initialize light value
+double currentTemp = 0;			// Initialize temperature value
+double currentLight = 0;		// Initialize light value
 
-//TODO: add values for temperature and light table
-// Temperature dependency values
-const int LOW_TEMP = -12;
+// Temperature - light intensity constants
+const int LOW_TEMP = -12;		// Temperature values
 const int MED_TEMP = 0;
 const int HIGH_TEMP = 20;
-// Light intensity dependency values
-const int MIN_LIGHT = 0;
+const int MIN_LIGHT = 0;		// Light intensity values
 const int LOW_LIGHT = 20;
 const int HIGH_LIGHT = 60;
 const int MAX_LIGHT = 100;
 
-
 void setup()
 {
-  Serial.begin(9600);
-  pinMode(TEMP_SENSOR, INPUT);	// set tempSensor as INPUT
-  pinMode(LIGHT_SENSOR, INPUT);	// set lightSensor as INPUT
+  Serial.begin(9600);			// set up serial communication
+  pinMode(TEMP_SENSOR, INPUT);		// set tempSensor as INPUT
+  pinMode(LIGHT_SENSOR, INPUT);		// set lightSensor as INPUT
   pinMode(RED_LED, OUTPUT);		// set redLed as OUTPUT
-  pinMode(GREEN_LED, OUTPUT);	// set greenLed as OUTPUT
+  pinMode(GREEN_LED, OUTPUT);		// set greenLed as OUTPUT
   pinMode(BLUE_LED, OUTPUT);		// set blueLed as OUTPUT
 }
 
 void loop()
 {
-  //TODO: add comments
-  currentTemp = toCelsius(analogRead(TEMP_SENSOR));
-  Serial.println(currentTemp);
+  currentTemp = toCelsius(analogRead(TEMP_SENSOR));			// takes temperature measurement
+  Serial.println(currentTemp);						// tests temperature measurement
+  currentLight = toLightPercentage(analogRead(LIGHT_SENSOR));		// takes light intensity measurement
+  Serial.println(currentLight);						// tests light intensity measurement
+  int ledToTurnOn = checkDependency(currentTemp, currentLight);		// use measurements taken to decide which light to turn on
   
-  currentLight = toLightPercentage(analogRead(LIGHT_SENSOR));
-  Serial.println(currentLight);
-  
-  int ledToTurnOn = checkDependency(currentTemp, currentLight);
-  
-  if (ledToTurnOn != ERROR)
+  if (ledToTurnOn != ERROR)						// handles undesirable errors
   {
-    PORTB = ALL_LOW;
-    digitalWrite(ledToTurnOn, HIGH);
+    PORTB = ALL_LOW;							// resets all pins
+    digitalWrite(ledToTurnOn, HIGH);					// turn on colored LED
   }
-  
-  delay(1000); // Wait for 1000 millisecond(s)
+  delay(1000);								// wait for 1 second
 }
 
-//TODO: add comments
-double toCelsius(double rawReading)
+double toCelsius(double rawReading)					// calculates the temperature in degres Celsius
 {
   return (rawReading*500/1023) - OFFSET;
 }
 
-//TODO: add comments
-double toLightPercentage(int rawReading)
+double toLightPercentage(int rawReading)				// calculates light intensity in percentage
 {
   return rawReading * K;
 }
     
-//TODO: add comments
-int checkDependency(double currentTemp, double currentLight)
+// checks the dependency between temperature and light intensity
+int checkDependency(double currentTemp, double currentLight)		
 {
 // Handles case if temperature lower than light (BLUE)
   if (	 (currentTemp < LOW_TEMP && currentLight > MIN_LIGHT)
@@ -95,6 +88,7 @@ int checkDependency(double currentTemp, double currentLight)
   {
 	return RED_LED;
   }
+  // Handles any undesirable outcome
   else
   { 
     return ERROR;
