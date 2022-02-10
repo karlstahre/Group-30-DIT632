fc8323557ef0cdbe4143d9bb44603ee79d91a200
Gianmarco Iachella <81154210+iachella@users.noreply.github.com>
2022-02-10 15:23:54 +0100

    Update exercise1.c

--- WP3/exercise1.c
+++ WP3/exercise1.c
@@ -1,32 +1,28 @@
-//TODO: add header comment with our names
-//TODO: make proper comments
-//TODO: translate to c code
+// (C) Maja Linder, Karl Stahre, Gianmarco Iachella group: 30 (2022)
+// Work package 3
+// Exercise 1
+// Submission code: 185160
 
 // constants won't change. They're used here to set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin12 =  12;      // the number of the LED pin
-const int ledPin13 = 13;
-
-const int ledPin =  12;// the number of the LED pin
+const int buttonPin = 2;	// the number of the pushbutton pin
+const int ledPin12 =  12;	// the number of the button-connected LED pin
+const int ledPin13 = 13;	// the number of the blinking LED pin
 
 // constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
+const long interval = 1000;	// interval at which to blink (milliseconds)
 
 // variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
+int buttonState = 0;		// variable for reading the pushbutton status
+int ledState = LOW;			// used to set ledPin13
 
-// Variables will change:
-int ledState = LOW;             // ledState used to set the LED
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
+unsigned long previousMillis = 0;	// will store last time ledPin13 was updated
 
 
 void setup() {
-  // initialize the LED pin as an output:
+  // initialize the LED pins as output
   pinMode(ledPin12, OUTPUT);
   pinMode(ledPin13, OUTPUT);
-  // initialize the pushbutton pin as an input:
+  // initialize the pushbutton pin as an input
   pinMode(buttonPin, INPUT);
 }
 
@@ -36,31 +32,41 @@ void loop() {
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
 
-  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-  if (buttonState == HIGH) {
-    // turn LED on:
+  // Handles case where the button is pressed:
+  if (buttonState == HIGH) 
+  {
+    // Turn LED on
     digitalWrite(ledPin12, HIGH);
-  } else {
-    // turn LED off:
+  }
+  // Handles case where button is not pressed
+  else 
+  {
+    // Turn LED off:
     digitalWrite(ledPin12, LOW);
   }
-  //TODO: connect button directly to led
+  
   // LED_PIN13
-  // here is where you'd put code that needs to be running all the time.
-  // check to see if it's time to blink the LED; that is, if the difference
-  // between the current time and last time you blinked the LED is bigger than
-  // the interval at which you want to blink the LED.
-  unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
+  
+  unsigned long currentMillis = millis(); // Store current time value
+ 
+  // Handles case where more than interval time has passed since last blink
+  if (currentMillis - previousMillis >= interval) 
+  {
+    // save the last time LED blinked
     previousMillis = currentMillis;
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
+    // Handles case where LED is off
+    if (ledState == LOW) 
+    {
+      // Set ledState to HIGH value
       ledState = HIGH;
-    } else {
+    } 
+    // Handles case where LED is on
+    else 
+    {
+      // Set ledState to LOW value
       ledState = LOW;
     }
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+    // Set ledPin13 with the relevant ledState
+    digitalWrite(ledPin13, ledState);
   }
 }
