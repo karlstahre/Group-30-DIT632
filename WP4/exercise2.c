31384d51a4e182b1cbe8018507e8260efa3f0030
Karl Stahre <60395651+karlstahre@users.noreply.github.com>
2022-02-18 09:15:31 +0100

    add submission and changes based on feedback

--- WP4/exercise2.c
+++ WP4/exercise2.c
@@ -1,16 +1,15 @@
 // (C) Maja Linder, Karl Stahre, Gianmarco Iachella group: 30 (2022)
 // Work package 4
 // Exercise 2
-// Submission code:
+// Submission code: 648664
 
 // Libraries
 #include <Servo.h>    // includes the Servo library which will be used to control the micro servo motor component
 
 // Macros
-#define HALF_MINUTE 30          // used for setting the direction and angle of the shaft
 #define ONE_MINUTE 60           // used for counting up to one minute
-#define HALF_MINUTE_MARK 180    // marks the degrees the servo shaft will be turned at the half minute mark
-#define MINUTE_MARK 0           // marks the degrees the servo shaft will be turned at the whole minute mark
+#define DOWN 180                // marks the degrees the servo shaft will be turned at the end of the minute
+#define UP 0                    // marks the degrees the servo shaft will be turned at the start of the minute
 #define PIN 3                   // the Arduino board pin connected to the micro servo motor
 #define SPEED 9600              // data rate in bits per second of serial data transmission
 #define MIN_PULSE_WIDTH 500     // the pulse width, in microseconds, corresponding to the angle on the servo at the whole minute mark (0 degrees)
@@ -35,17 +34,11 @@ void loop()
 {
   servo_9.write(pos);                                                              // sets the angle of the servo
   printSeconds();                                                                  // prints the seconds of the timer
+  
+  pos = map(counter, 0, ONE_MINUTE, UP, DOWN);                                     //maps the angle of the shaft
   if (counter == ONE_MINUTE)                                                       // if the counter has counted one full minute
   {
     counter = 0;                                                                   // reset counter to zero
-  }
-  if (counter <= HALF_MINUTE)                                                      // if the counter is between 0 and 30
-  {
-    pos = map(counter, 0, HALF_MINUTE, MINUTE_MARK, HALF_MINUTE_MARK);             //maps the angle of the shaft for the first half minute
-  }
-  else
-  {
-    pos = map(counter, HALF_MINUTE, ONE_MINUTE, HALF_MINUTE_MARK, MINUTE_MARK);    // maps the angle of the shaft for the second half minute
   }
 }
 
